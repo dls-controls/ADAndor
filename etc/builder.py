ea247954b6c97ee65eba84e542c29d1fada011e1
Alan Greer <alan.greer@diamond.ac.uk>
2015-10-08 16:03:30 +0100

    Applied DLS specific configuration changes and builder support.

---
+++ etc/builder.py
@@ -0,0 +1,86 @@
+from iocbuilder import Device, AutoSubstitution, Architecture, SetSimulation
+from iocbuilder.arginfo import *
+from iocbuilder.modules.asyn import Asyn, AsynPort, AsynIP
+
+from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, NDFileTemplate, makeTemplateInstance, includesTemplates
+
+@includesTemplates(ADBaseTemplate, NDFileTemplate)
+class andorCCDTemplate(AutoSubstitution):
+#    TemplateFile = "andorCCD.template"
+#    SubstitutionOverwrites = [_ADBaseTemplate]
+    TemplateFile = "andorCCD.template"
+#    SubstitutionOverwrites = [_NDFile]
+
+class AndorSpecificMustBeLoadedFirst(Device):
+    """Library dependencies for areaDetector"""
+    LibFileList = ['andor']
+    AutoInstantiate = True
+
+class andorCCD(AsynPort):
+    """Creates a andorCCD camera areaDetector driver"""
+    Dependencies = (AndorSpecificMustBeLoadedFirst, ADCore)
+    # This tells xmlbuilder to use PORT instead of name as the row ID
+    UniqueName = "PORT"
+    _SpecificTemplate = andorCCDTemplate
+    def __init__(self, PORT, SHAMROCKID=0, BUFFERS = 50, MEMORY = 0, INSTALLPATH = "", PRIORITY = 0, STACKSIZE = 100000, **args):
+        print "*****************************************************"
+        print "WARNING - your builder generated andor IOC may not work!"
+        print " Currently it is necessary to modify the generated iocs src/makefile to ensure the following link order:"
+        print " BLxxI-EA-IOC-01_LIBS += ADBase, BLxxI-EA-IOC-01_LIBS += andor, BLxxI-EA-IOC-01_LIBS += GraphicsMagick"
+        print " If you are using a plugin that requires GraphicsMagick (eg URLDriver or NDFileMagick) then it may not work."
+        print " There is a conflict with a SetImage() function exisiting in both the andor and GraphicsMagick libraries"
+        print " See note in areaDetector/etc/builder.py regarding linking the andor library for more info."
+        print "*****************************************************"    
+        # Init the superclass (AsynPort)
+        self.__super.__init__(PORT)
+        # Update the attributes of self from the commandline args
+        self.__dict__.update(locals())
+        # Make an instance of our template
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
+
+    # __init__ arguments
+    ArgInfo = ADBaseTemplate.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
+        PORT = Simple('Port name for the detector', str),
+        SHAMROCKID = Simple('shamrockID The index number of the Shamrock spectrograph, if installed.', int),
+        BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
+            'plugin callbacks', int),
+        MEMORY = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
+            'for driver and all attached plugins', int),
+        INSTALLPATH = Simple('The path to the Andor directory containing the detector INI files, etc.'
+            'This can be specified as an empty string ("") for new detectors that do not use the INI'
+            'files on Windows, but must be a valid path on Linux (probably /usr/local/etc/andor).', str),
+        PRIORITY = Simple('The thread priority for the asyn port driver thread', int),
+        STACKSIZE = Simple('The stack size for the asyn port driver thread', int))
+
+    # Device attributes
+    LibFileList = ['andorCCD', 'andor', 'shamrockcif']
+    DbdFileList = ['andorCCDSupport', 'shamrockSupport']
+
+    def Initialise(self):
+        print '# andorCCDConfig(portName, installPath, shamrockID, maxBuffers, maxMemory, priority, stackSize)'
+        print 'andorCCDConfig("%(PORT)s", "%(INSTALLPATH)s", %(SHAMROCKID)d, %(BUFFERS)d, %(MEMORY)d, %(PRIORITY)d, %(STACKSIZE)d)' \
+            % self.__dict__
+        #print '# andorCCDConfig(portName, maxBuffers, maxMemory, installPath, priority, stackSize)'
+        #print 'andorCCDConfig("%(PORT)s", %(BUFFERS)d, %(MEMORY)d, "%(INSTALLPATH)s", %(PRIORITY)d, %(STACKSIZE)d)' \
+        #    % self.__dict__
+
+# ModuleBase.__AggregateDependencies unhelpfully puts AreaDetector and its Dependencies first on the list as _ADBase is a subclass of andorCCD.
+# Override this behaviour and set the Dependencies to what we think they should actually be
+#andorCCD.Dependencies = (AndorSpecificMustBeLoadedFirst, AreaDetector)
+
+# NB1 The 2-96 verion of libandor includes definitions for TiXmlDocument::LoadFile which conflicts with the exisiting TiXmlDocument library
+# already in areadDetector. Andor need to remove this from their library exports until that happens we must link libandor after ADBase
+# (ie BL08I-EA-IOC-01_LIBS += ADBase must be before BL08I-EA-IOC-01_LIBS += andor in the ioc src/makefile)
+# The above hack performs this re-ordering.
+
+# NB2 The andor library includes a definition for function SetImage. Unfortunately GraphicsMagik also includes a SetImage funtion which conflicts.
+# For andor to work we need to load libandor before the GraphicsMajic library. This may break something; possibly the URL loading plugin or one  the file save plugins?
+# (ie. BL08I-EA-IOC-01_LIBS += andor must be before BL08I-EA-IOC-01_LIBS += GraphicsMagick in the ioc src/makefile
+# The above hack DOES NOT DO THIS RE-ORDERING.
+
+def andorCCD_sim(**kwargs):
+    return simDetector(1004, 1004, **kwargs)
+
+SetSimulation(andorCCD, andorCCD_sim)
+
+
